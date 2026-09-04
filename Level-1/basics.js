b357954c09d7b8e5439a43c9fc89671e3b2d588d
itzSumma <itzSumma11@gmail.com>
2026-09-04 20:48:09 +0600

    feat: add average function to calculate the average of three numbers

--- Level-1/basics.js
+++ Level-1/basics.js
@@ -4,3 +4,9 @@ function sum(a, b) {
 }
 
 console.log(sum(10, 20)); // 30
+//Problem 02: Find Average
+function average(a, b, c) {
+  return (a + b + c) / 3;
+}
+
+console.log(average(10, 20, 30)); // 20
