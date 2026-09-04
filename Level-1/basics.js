0ea057d3c36e53737faa7d1153247172b7fe6b08
itzSumma <itzSumma11@gmail.com>
2026-09-04 20:49:24 +0600

    feat: add function to convert Celsius to Fahrenheit

--- Level-1/basics.js
+++ Level-1/basics.js
@@ -10,3 +10,11 @@ function average(a, b, c) {
 }
 
 console.log(average(10, 20, 30)); // 20
+
+//Problem 03: Convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0)); // 32
+console.log(celsiusToFahrenheit(100)); // 212
