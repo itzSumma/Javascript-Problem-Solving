3f0d9dc8edf90d751c9aee7d2bb2c0083ceb449f
itzSumma <itzSumma11@gmail.com>
2026-09-06 12:04:35 +0600

    refactor: clean up comments and ensure consistent formatting in Conditions.js

--- Level-1/Conditions.js
+++ Level-1/Conditions.js
@@ -1,4 +1,4 @@
-//Problem 06: Check Even or Odd
+// Check Even or Odd
 function checkEvenOdd(number) {
   if (number % 2 === 0) {
     return "Even";
@@ -9,3 +9,18 @@ function checkEvenOdd(number) {
 
 console.log(checkEvenOdd(10)); // Even
 console.log(checkEvenOdd(11)); // Odd
+
+//Check Positive, Negative or Zero
+function checkNumber(number) {
+  if (number > 0) {
+    return "Positive";
+  } else if (number < 0) {
+    return "Negative";
+  }
+
+  return "Zero";
+}
+
+console.log(checkNumber(-5)); // Negative
+console.log(checkNumber(5)); // Positive
+console.log(checkNumber(0)); // Zero    
