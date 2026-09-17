f76dc50a45f2f7a5996009b930b9f6c9d108c6d9
itzSumma <itzSumma11@gmail.com>
2026-09-17 18:42:30 +0600

    feat: add function to reverse a string

---
+++ Level-1/String.js
@@ -0,0 +1,12 @@
+//Reverse a String
+function reverseString(text) {
+  let reversed = "";
+
+  for (let i = text.length - 1; i >= 0; i--) {
+    reversed += text[i];
+  }
+
+  return reversed;
+}
+
+console.log(reverseString("hello")); // olleh
